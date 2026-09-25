ff96fe1f011b5000b8f3534679e5c381f4f8194e
robinr2 <57617379+robinr2@users.noreply.github.com>
2026-09-26 00:18:12 +0200

    Add dependency-cruiser with stable dependencies rule

---
+++ server/.dependency-cruiser.cjs
@@ -0,0 +1,173 @@
+module.exports = {
+  forbidden: [
+    {
+      name: 'no-circular',
+      severity: 'error',
+      from: {},
+      to: {
+        circular: true,
+      },
+    },
+    {
+      name: 'no-orphans',
+      severity: 'error',
+      from: {
+        orphan: true,
+        pathNot: [
+          '(^|/)[.][^/]+[.](?:js|cjs|mjs|ts|cts|mts|json)$',
+          '[.]d[.]ts$',
+          '(^|/)tsconfig[.]json$',
+          '(^|/)(?:babel|webpack)[.]config[.](?:js|cjs|mjs|ts|cts|mts|json)$',
+        ],
+      },
+      to: {},
+    },
+    {
+      name: 'no-deprecated-core',
+      severity: 'error',
+      from: {},
+      to: {
+        dependencyTypes: ['core'],
+        path: [
+          '^v8/tools/codemap$',
+          '^v8/tools/consarray$',
+          '^v8/tools/csvparser$',
+          '^v8/tools/logreader$',
+          '^v8/tools/profile_view$',
+          '^v8/tools/profile$',
+          '^v8/tools/SourceMap$',
+          '^v8/tools/splaytree$',
+          '^v8/tools/tickprocessor-driver$',
+          '^v8/tools/tickprocessor$',
+          '^node-inspect/lib/_inspect$',
+          '^node-inspect/lib/internal/inspect_client$',
+          '^node-inspect/lib/internal/inspect_repl$',
+          '^async_hooks$',
+          '^punycode$',
+          '^domain$',
+          '^constants$',
+          '^sys$',
+          '^_linklist$',
+          '^_stream_wrap$',
+        ],
+      },
+    },
+    {
+      name: 'not-to-deprecated',
+      severity: 'error',
+      from: {},
+      to: {
+        dependencyTypes: ['deprecated'],
+      },
+    },
+    {
+      name: 'no-non-package-json',
+      severity: 'error',
+      from: {},
+      to: {
+        dependencyTypes: ['npm-no-pkg', 'npm-unknown'],
+      },
+    },
+    {
+      name: 'not-to-unresolvable',
+      severity: 'error',
+      from: {},
+      to: {
+        couldNotResolve: true,
+      },
+    },
+    {
+      name: 'no-duplicate-dep-types',
+      severity: 'error',
+      from: {},
+      to: {
+        moreThanOneDependencyType: true,
+        dependencyTypesNot: ['type-only'],
+      },
+    },
+    {
+      name: 'not-to-test',
+      severity: 'error',
+      from: {
+        pathNot: '^(test)',
+      },
+      to: {
+        path: '^(test)',
+      },
+    },
+    {
+      name: 'not-to-spec',
+      severity: 'error',
+      from: {},
+      to: {
+        path: '[.](?:spec|test)[.](?:js|mjs|cjs|jsx|ts|mts|cts|tsx)$',
+      },
+    },
+    {
+      name: 'not-to-dev-dep',
+      severity: 'error',
+      from: {
+        path: '^(src)',
+        pathNot: '[.](?:spec|test)[.](?:js|mjs|cjs|jsx|ts|mts|cts|tsx)$',
+      },
+      to: {
+        dependencyTypes: ['npm-dev'],
+        dependencyTypesNot: ['type-only'],
+        pathNot: ['node_modules/@types/'],
+      },
+    },
+    {
+      name: 'optional-deps-used',
+      severity: 'error',
+      from: {},
+      to: {
+        dependencyTypes: ['npm-optional'],
+      },
+    },
+    {
+      name: 'peer-deps-used',
+      severity: 'error',
+      from: {},
+      to: {
+        dependencyTypes: ['npm-peer'],
+      },
+    },
+    {
+      name: 'no-unstable-dependencies',
+      severity: 'error',
+      from: {},
+      to: {
+        moreUnstable: true,
+      },
+    },
+  ],
+  options: {
+    doNotFollow: {
+      path: ['node_modules'],
+    },
+    detectProcessBuiltinModuleCalls: true,
+    tsPreCompilationDeps: true,
+    tsConfig: {
+      fileName: 'tsconfig.build.json',
+    },
+    enhancedResolveOptions: {
+      exportsFields: ['exports'],
+      conditionNames: ['import', 'require', 'node', 'default', 'types'],
+      extensions: ['.ts', '.d.ts'],
+      mainFields: ['module', 'main', 'types', 'typings'],
+    },
+    skipAnalysisNotInRules: true,
+    reporterOptions: {
+      dot: {
+        collapsePattern: 'node_modules/(?:@[^/]+/[^/]+|[^/]+)',
+      },
+      archi: {
+        collapsePattern:
+          '^(?:packages|src|lib(s?)|app(s?)|bin|test(s?)|spec(s?))/[^/]+|node_modules/(?:@[^/]+/[^/]+|[^/]+)',
+      },
+      text: {
+        highlightFocused: true,
+      },
+    },
+  },
+};
