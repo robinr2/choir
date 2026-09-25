364f93cf980668abdc772165bc3cd19c3730c6b1
robinr2 <57617379+robinr2@users.noreply.github.com>
2026-09-25 23:30:30 +0200

    Compile tests with SWC and require full coverage

--- server/vitest.config.e2e.ts
+++ server/vitest.config.e2e.ts
@@ -1,8 +1,9 @@
+import swc from 'unplugin-swc';
 import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [tsconfigPaths(), swc.vite({ module: { type: 'es6' } })],
   test: {
     globals: true,
     root: './',
--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -1,13 +1,22 @@
+import swc from 'unplugin-swc';
 import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  // Resolves the path aliases declared in tsconfig.json, including the ones
-  // added by `nest g library`.
-  plugins: [tsconfigPaths()],
+  plugins: [tsconfigPaths(), swc.vite({ module: { type: 'es6' } })],
   test: {
     globals: true,
     root: './',
     include: ['**/*.spec.ts'],
+    coverage: {
+      include: ['src/**/*.ts'],
+      exclude: ['src/**/*.spec.ts', 'src/main.ts'],
+      thresholds: {
+        lines: 100,
+        functions: 100,
+        branches: 100,
+        statements: 100,
+      },
+    },
   },
 });
